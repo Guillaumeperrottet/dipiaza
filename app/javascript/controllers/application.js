3f6d1013522eaf769924fc7c100d3a432781b2f4
guillaume perrottet <perrottet.guillaume.97@gmail.com>
2025-01-13 17:32:45 +0100

    navbar

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,22 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const navbar = document.querySelector(".navbar");
+  let lastScrollPosition = 0;
+
+  window.addEventListener("scroll", () => {
+    const currentScrollPosition = window.scrollY;
+
+    if (currentScrollPosition > lastScrollPosition) {
+      // Masque la navbar quand on défile vers le bas
+      navbar.style.top = "-80px";
+    } else {
+      // Affiche la navbar quand on défile vers le haut
+      navbar.style.top = "0";
+    }
+
+    lastScrollPosition = currentScrollPosition;
+  });
+});
