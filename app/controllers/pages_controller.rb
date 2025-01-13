d280a5fd6ba89e81cccaa35decce90b4a59c80ef
guillaume perrottet <perrottet.guillaume.97@gmail.com>
2025-01-13 16:16:15 +0100

    navbar début

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def notre_histoire
+  end
+
+  def nos_engagements
+  end
+
+  def nos_restaurants
+  end
+
+  def nous_rejoindre
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root "pages#home" # Page d'accueil
+  get "notre_histoire", to: "pages#notre_histoire"
+  get "nos_engagements", to: "pages#nos_engagements"
+  get "nos_restaurants", to: "pages#nos_restaurants"
+  get "nous_rejoindre", to: "pages#nous_rejoindre"
 end
