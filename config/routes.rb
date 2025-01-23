11680cf80ed2082efbc250deca9b25883e00c846
guillaume perrottet <perrottet.guillaume.97@gmail.com>
2025-01-23 09:56:21 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "pages#home" # Page d'accueil
   get "notre_histoire", to: "pages#notre_histoire"
-  get "nos_engagements", to: "pages#nos_engagements"
+  get "nos_valeurs", to: "pages#nos_valeurs"
   get "nos_restaurants", to: "pages#nos_restaurants"
   get "nous_rejoindre", to: "pages#nous_rejoindre"
 end
